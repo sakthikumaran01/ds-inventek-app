3c945a2c4e6eb3f56ab913bc512afe59ec54413e
sakthikumaran01 <sakthisk1999@gmail.com>
2026-06-14 03:22:24 +0530

    Fix layout compilation error by removing undefined SpeedInsights

--- app/layout.jsx
+++ app/layout.jsx
@@ -78,7 +78,6 @@ export default function RootLayout({ children }) {
         <FloatingWhatsApp />
         <BackToTop />
         <CustomCursor />
-        <SpeedInsights />
       </body>
     </html>
   );
